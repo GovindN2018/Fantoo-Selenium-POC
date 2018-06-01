eefbbb02a81b4bd4bf6ce314d628dd71a62fa961
Govind N <govind.nayak@outlook.com>
2018-06-01 15:55:17 +0100

    Removed unwanted comments from EntryPoint.cs

--- ConsoleApp1/EntryPoint.cs
+++ ConsoleApp1/EntryPoint.cs
@@ -2,22 +2,6 @@
 {
     public class Program
     {
-        static void Main()
-        {
-            //NavigateTo.NavigateToProductPage();
-
-            //Thread.Sleep(1000);
-
-            //NavigateTo.NavigateToAboutPage();
-
-            //Thread.Sleep(1000);
-
-            //NavigateTo.NavigateToFreeTrialPage();
-
-            //Thread.Sleep(1000);
-
-            //NavigateTo.NavigateToHomePage();
-
-        }
+        static void Main() {}
     }
 }

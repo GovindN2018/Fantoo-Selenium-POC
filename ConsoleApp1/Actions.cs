383a61fc35821e50f7469130f148a373043a9767
Govind N <govind.nayak@outlook.com>
2018-06-03 23:51:20 +0100

    Refactored Actions.cs class

--- ConsoleApp1/Actions.cs
+++ ConsoleApp1/Actions.cs
@@ -18,32 +18,32 @@ namespace AutoTestFramework
         {
             FantooContactUsPage cuClear = new FantooContactUsPage();
 
-            cuClear.TbFirstName.Clear();
-            cuClear.TbLastName.Clear();
-            cuClear.TbCompanyName.Clear();
-            cuClear.TbEmailAddress.Clear();
-            cuClear.TbPhoneNumber.Clear();
-            cuClear.DrbSubject.SendKeys("Select an option" + Keys.Enter);
-            cuClear.TbMessage.Clear();
+            cuClear.TextFirstName.Clear();
+            cuClear.TextLastName.Clear();
+            cuClear.TextCompanyName.Clear();
+            cuClear.TextEmailAddress.Clear();
+            cuClear.TextPhoneNumber.Clear();
+            cuClear.ListSubject.SendKeys("Select an option" + Keys.Enter);
+            cuClear.TextMessage.Clear();
         }
 
         public static void FillContactUsForm(string FirstName, string LastName, string CompanyName, string EmailAddress, string PhoneNumber, string Subject, string Message)
         {
             FantooContactUsPage cuPost = new FantooContactUsPage();
 
-            cuPost.TbFirstName.SendKeys(FirstName);
-            cuPost.TbLastName.SendKeys(LastName);
-            cuPost.TbCompanyName.SendKeys(CompanyName);
-            cuPost.TbEmailAddress.SendKeys(EmailAddress);
-            cuPost.TbPhoneNumber.SendKeys(PhoneNumber);
-            cuPost.DrbSubject.SendKeys(Subject + Keys.Enter);
-            cuPost.TbMessage.SendKeys(Message);        
+            cuPost.TextFirstName.SendKeys(FirstName);
+            cuPost.TextLastName.SendKeys(LastName);
+            cuPost.TextCompanyName.SendKeys(CompanyName);
+            cuPost.TextEmailAddress.SendKeys(EmailAddress);
+            cuPost.TextPhoneNumber.SendKeys(PhoneNumber);
+            cuPost.ListSubject.SendKeys(Subject + Keys.Enter);
+            cuPost.TextMessage.SendKeys(Message);        
         }
 
         public static void SubmitContactUsForm()
         {
             FantooContactUsPage cuSubmit = new FantooContactUsPage();
-            cuSubmit.BtnSubmit.Click();
+            cuSubmit.ButtonSubmit.Click();
         }
 
     }
--- ConsoleApp1/Pages/FantooContactUsPage.cs
+++ ConsoleApp1/Pages/FantooContactUsPage.cs
@@ -11,64 +11,64 @@ namespace AutoTestFramework.Pages
         }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > label")]
-        public IWebElement LblName { get; set; }
+        public IWebElement LabelName { get; set; }
 
         [FindsBy(How = How.Id, Using = "first-name")]
-        public IWebElement TbFirstName { get; set; }
+        public IWebElement TextFirstName { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > div > div.left-input.has-error > span")]
         public IWebElement WarningNoFirstName { get; set; }
 
         [FindsBy(How = How.Id, Using = "last-name")]
-        public IWebElement TbLastName { get; set; }
+        public IWebElement TextLastName { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > div > div.right-input.has-error > span")]
         public IWebElement WarningNoLastName { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(1) > label")]
-        public IWebElement LblCompanyName { get; set; }
+        public IWebElement LabelCompanyName { get; set; }
 
         [FindsBy(How = How.Id, Using = "company-name")]
-        public IWebElement TbCompanyName { get; set; }
+        public IWebElement TextCompanyName { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li.has-error > span")]
         public IWebElement WarningNoCompanyName { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(2) > label")]
-        public IWebElement LblEmailAddress { get; set; }
+        public IWebElement LabelEmailAddress { get; set; }
 
         [FindsBy(How = How.Id, Using = "email-address")]
-        public IWebElement TbEmailAddress { get; set; }
+        public IWebElement TextEmailAddress { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(2) > span")]
         public IWebElement WarningInvalidOrBlankEmail { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(3) > label")]
-        public IWebElement LblPhoneNumber { get; set; }
+        public IWebElement LabelPhoneNumber { get; set; }
 
         [FindsBy(How = How.Id, Using = "phone-number")]
-        public IWebElement TbPhoneNumber { get; set; }
+        public IWebElement TextPhoneNumber { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(4) > label")]
-        public IWebElement LblSubject { get; set; }
+        public IWebElement LabelSubject { get; set; }
           
         [FindsBy(How = How.Id, Using = "subject")]
-        public IWebElement DrbSubject { get; set; }
+        public IWebElement ListSubject { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(4) > span")]
         public IWebElement WarningNoSubject { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(5) > label")]
-        public IWebElement LblMessage { get; set; }
+        public IWebElement LabelMessage { get; set; }
 
         [FindsBy(How = How.Id, Using = "message")]
-        public IWebElement TbMessage { get; set; }
+        public IWebElement TextMessage { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(5) > span")]
         public IWebElement WarningNoMessage{ get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "#form-inner > ul > li:nth-child(6) > input[type=\"submit\"]")]
-        public IWebElement BtnSubmit { get; set; }
+        public IWebElement ButtonSubmit { get; set; }
 
         [FindsBy(How = How.Id, Using = "success-message")]
         public IWebElement SuccessMessage { get; set; }
